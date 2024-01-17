df838fa74b230059c49b19a0b0ec0f846847dd51
fred-chen <fred_chen@trendmicro.com>
2024-01-17 08:48:38 +0800

    Add new api /ubtv2

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 
 	server := gin.Default()
 	server.GET("/ubt", ubt)
+	server.GET("/ubtv2", ubtV2)
 	server.Run(":8888")
 }
 
@@ -57,6 +58,23 @@ func ubt(c *gin.Context) {
 		return
 	}
 	ubtTracingData(startTime, endTime, int(cnt))
+}
+
+func ubtV2(c *gin.Context) {
+	trafficPerMin, _ := c.GetQuery("traffic_per_min")
+	duration, _ := c.GetQuery("duration")
+	cnt, _ := strconv.ParseInt(trafficPerMin, 10, 64)
+	if cnt == 0 {
+		cnt = 70000
+	}
+	dcnt, _ := strconv.ParseInt(duration, 10, 64)
+	if dcnt == 0 {
+		dcnt = 3600
+	}
+	for i := 0; i < int(dcnt); i++ {
+		sendUbtTraceCurrentTimeToIPP(time.Now(), int(cnt))
+		time.Sleep(1 * time.Minute)
+	}
 }
 
 func ubtTracingData(startTime, endTime string, trafficPerMin int) {
@@ -85,7 +103,8 @@ func sendUbtTraceToIPP(t time.Time, trafficPerMin int) {
 		st := traceData.Timestamp.Add(time.Duration(rand.Intn(60)) * time.Second)
 		et := st.Add(time.Duration(traceData.ResponseTime) * time.Millisecond)
 		_, span := otel.Tracer(traceData.ServiceName).Start(context.Background(), operationName,
-			go_trace.WithTimestamp(st))
+			go_trace.WithTimestamp(st),
+		)
 
 		span.SetAttributes(attribute.KeyValue{
 			Key:   attribute.Key("x-customer-id"),
@@ -109,5 +128,38 @@ func sendUbtTraceToIPP(t time.Time, trafficPerMin int) {
 		})
 
 		span.End(go_trace.WithTimestamp(et))
+
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func sendUbtTraceCurrentTimeToIPP(t time.Time, trafficPerMin int) {
+	for i := 0; i < trafficPerMin; i++ {
+		traceData := lib.GenRawData(t)
+		operationName := "local-test"
+		_, span := otel.Tracer(traceData.ServiceName).Start(context.Background(), operationName)
+
+		span.SetAttributes(attribute.KeyValue{
+			Key:   attribute.Key("x-customer-id"),
+			Value: attribute.StringValue(traceData.CustomerID),
+		})
+		span.SetAttributes(attribute.KeyValue{
+			Key:   attribute.Key("x-cpid"),
+			Value: attribute.StringValue(traceData.Cpid),
+		})
+		span.SetAttributes(attribute.KeyValue{
+			Key:   attribute.Key("http.url"),
+			Value: attribute.StringValue(traceData.API),
+		})
+		span.SetAttributes(attribute.KeyValue{
+			Key:   attribute.Key("http.method"),
+			Value: attribute.StringValue(traceData.HTTPMethod),
+		})
+		span.SetAttributes(attribute.KeyValue{
+			Key:   attribute.Key("http.status_code"),
+			Value: attribute.StringValue(traceData.HTTPStatusCode),
+		})
+
+		span.End()
 	}
 }
